c9396ab7c59c1214caa66465e9ee48ca80788702
pashqa <pashqevi4@gmail.com>
2023-03-19 09:59:22 +0700

    Task 1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Enter Your Number");
+int x = Convert.ToInt32(Console.ReadLine());
+int a = x / 1000;
+int b = x % 100;
+if((a % 10 == b / 10) && (a / 10 == b % 10))
+    Console.WriteLine("Your number is a palindrome");
+else
+    Console.WriteLine("Your number is not a palindrome");
